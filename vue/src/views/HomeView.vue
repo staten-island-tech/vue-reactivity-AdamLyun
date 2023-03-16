b8d3d73fa67cfc7d64bd355c378e79122fa75a21
AdamLyun <adamlyun@gmail.com>
2023-03-16 13:00:58 -0400

    Revert "aedszfxg"
    This reverts commit 01e665c96707dadab111e9d978c2884ee9566a99.

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,47 +1,9 @@
-<script>
-export default {
-  name: 'Home',
-  components: {},
-  data() {
-    return {
-      student: 'Jeffrey',
-      passed: true,
-      animals: ['Yes', 'No', 'Maybe', 'So'],
-      loggedIn: true,
-      message: '',
-      selected: ''
-    }
-  },
-  methods: {
-    authState: function () {
-      if (this.loggedIn === false) {
-        this.loggedIn = true
-      } else {
-        this.loggedIn = false
-      }
-    }
-  }
-}
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <h1 v-if="passed">{{ student }}</h1>
-    <h1 v-else>dke</h1>
-    <ul>
-      <li v-for="animal in animals" :key="animal">{{ animal }}</li>
-    </ul>
-
-    <button @click="authState" v-if="loggedIn">Logout</button>
-    <button @click="authState" v-else="loggedIn">Login</button>
-    <input type="text" placeholder="EEEEEEEEEEEE" v-model="message" />
-
-    <select v-model="selected">
-      <option>A</option>
-      <option>B</option>
-      <option>C</option>
-    </select>
-    <p>{{ message }}</p>
-    <p>{{ selected }}</p>
+    <TheWelcome />
   </main>
 </template>
