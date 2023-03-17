e71677411588cd2f89aa5e683bf84392332dd01c
AdamLyun <adamlyun@gmail.com>
2023-03-17 13:16:09 -0400

    RSFHBs;lkbnsk.nbks;.f

--- vue/src/components/icons/FactualCard.vue
+++ vue/src/components/icons/FactualCard.vue
@@ -1,9 +1,28 @@
 <template>
-  <div></div>
+  <div class="cardfr">
+<h2>{{ Name }}</h2>
+<p>{{ description }}</p>
+
+
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+name: "FactualCard",
+props:{
+  Name: String,
+    description: String,
+}
+
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cardfr {
+  width: 30%;
+  background-color: blue;
+  height: 500px;
+  margin: 10px auto;
+}
+</style>
