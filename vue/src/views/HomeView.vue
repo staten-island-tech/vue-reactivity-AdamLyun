8fdf122cc64c1bcf5da26bfa0c49fa00a9803e0e
AdamLyun <adamlyun@gmail.com>
2023-03-31 13:17:53 -0400

    as

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 import FactualCard from '../components/FactualCard.vue'
 
 import champions from '../components/array.js'
@@ -13,14 +13,45 @@ export default {
       list: champions,
       filter_arr: ['Assassin', 'Mage', 'Marksman', 'Mage', 'Support', 'Tank']
     }
+  },
+
+  method: {
+    fitlerClass(champion) {
+      console.log(champion)
+    }
   }
 }
+</script> -->
+
+<script setup>
+import FactualCard from '../components/FactualCard.vue'
+
+import champions from '../components/array.js'
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const filter_arr = ref(['Assassin', 'Mage', 'Marksman', 'Mage', 'Support', 'Tank'])
+
+const allChamps = ref(champions)
+
+// functions that mutate state and trigger updates
+function filterClass(champion) {
+  console.log(champion)
+  champions.filter((champ) => champ.tags.includes(champion))
+  console.log(champions
+  )
+  // allChamps = allChamps.filter((champ) => champ.tags.match(champion))
+}
+// lifecycle hooks
+onMounted(() => {
+  console.log(filter_arr)
+})
 </script>
 
 <template>
   <main>
-    <div class="EricLin" v-for="lol in filter_arr">
-      <button @click="filter" class="familyfriendly">{{ lol }}</button>
+    <div class="EricLin" v-for="champ in filter_arr">
+      <button @click="filterClass(champ)" class="familyfriendly">{{ champ }}</button>
     </div>
 
     <div id="container-box">
@@ -37,9 +68,29 @@ export default {
 </template>
 <style>
 .EricLin {
-  flex-wrap: wrap;
-  justify-content: center;
   display: flex;
-  margin: 2%;
+  flex-direction: column;
+  align-items: center;
+}
+.familyfriendly,
+.familyfriendly:link,
+.familyfriendly:visited {
+  background: none;
+  border: none;
+  cursor: pointer;
+  line-height: 1.5;
+  font: 700 1.2rem 'Roboto Slab', sans-serif;
+  padding: 0.75em 2em;
+  letter-spacing: 0.05rem;
+  margin: 1em;
+  width: 13rem;
+  background-color: green;
+  transition: all 0.2s;
+  color: #58afd1;
+  border-radius: 1rem;
+}
+.familyfriendly:hover {
+  transform: translateY(-10px);
+  box-shadow: 0 10px 0.5rem rgba(0, 0, 0, 0.2);
 }
 </style>
